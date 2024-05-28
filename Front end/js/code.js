840d7a7e1063bbd1cd3fe7c1e23717e5792b49f6
Tyler Knoop <MonotudeBusiness@gmail.com>
2024-05-28 00:27:55 -0400

    contacts changes

--- Front end/js/code.js
+++ Front end/js/code.js
@@ -24,13 +24,17 @@ function doLogin()
 	let xhr = new XMLHttpRequest();
 	xhr.open("POST", url, true);
 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-	try {
-		xhr.onreadystatechange = function () {
-			if (this.readyState == 4 && this.status == 200) {
+	try 
+	{
+		xhr.onreadystatechange = function () 
+		{
+			if (this.readyState == 4 && this.status == 200) 
+				{
 				let jsonObject = JSON.parse(xhr.responseText);
 				userId = jsonObject.id;
 
-				if (userId < 1) {
+				if (userId < 1) 
+					{
 					document.getElementById("response").innerHTML = "Invalid username or password";
 					return;
 				}
@@ -38,18 +42,57 @@ function doLogin()
 				firstName = jsonObject.firstName;
 				lastName = jsonObject.lastName;
 
+				saveCookie();
+
 				window.location.href = "contacts.html";
 			}
 		};
 		xhr.send(jsonPayload);
 	}
-	catch (err) {
+	catch (err) 
+	{
 		document.getElementById("loginResult").innerHTML = err.message;
 	}
 
 }
 
-function doRegister() {
+function saveCookie() 
+{
+	let minutes = 20;
+	let date = new Date();
+	date.setTime(date.getTime() + (minutes * 60 * 1000));
+	document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
+}
+
+function readCookie() 
+{
+	userId = -1;
+	let data = document.cookie;
+	let splits = data.split(",");
+	for (var i = 0; i < splits.length; i++) {
+		let thisOne = splits[i].trim();
+		let tokens = thisOne.split("=");
+		if (tokens[0] == "firstName") {
+			firstName = tokens[1];
+		}
+		else if (tokens[0] == "lastName") {
+			lastName = tokens[1];
+		}
+		else if (tokens[0] == "userId") {
+			userId = parseInt(tokens[1].trim());
+		}
+	}
+
+	if (userId < 0) {
+		window.location.href = "index.html";
+	}
+	else {
+		//		document.getElementById("userName").innerHTML = "Logged in as " + firstName + " " + lastName;
+	}
+}
+
+function doRegister() 
+{
 	userId = 0;
 	let firstName = document.getElementById("firstName").value;
 	let lastName = document.getElementById("lastName").value;
@@ -70,13 +113,13 @@ function doRegister() {
 
 	if (login == "") 
 	{
-		document.getElementById("registerResult").innerHTML = "Please input a login";
+		document.getElementById("response").innerHTML = "Please input a login";
 		return;
 	}
 
 	if (password == "") 
 	{
-		document.getElementById("registerResult").innerHTML = "Please input a password";
+		document.getElementById("response").innerHTML = "Please input a password";
 		return;
 	}
 
@@ -97,4 +140,91 @@ function doRegister() {
 	{
 		document.getElementById("response").innerHTML = err.message;
 	}
+}
+
+function loadName()
+{
+	document.getElementById("greeting").innerHTML = "Hello, " + firstName + " " + lastName + "!";
+}
+
+function doLogout() 
+{
+	userId = 0;
+	firstName = "";
+	lastName = "";
+	document.cookie = "firstName= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+	window.location.href = "index.html";
+}
+
+function addContact()
+{
+	let newColor = document.getElementById("colorText").value;
+	document.getElementById("colorAddResult").innerHTML = "";
+
+	//^\d{3}\-\d{3}\-\d{4}$ phone number regex
+
+	///^[^\s@]+@[^\s@]+\.[^\s@]+$/ email regex
+
+	let tmp = {color:newColor,userId,userId};
+	let jsonPayload = JSON.stringify( tmp );
+
+	let url = urlBase + '/AddColor.' + extension;
+	
+	let xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function() 
+		{
+			if (this.readyState == 4 && this.status == 200) 
+			{
+				document.getElementById("colorAddResult").innerHTML = "Color has been added";
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		document.getElementById("colorAddResult").innerHTML = err.message;
+	}
+}
+
+function searchContacts() 
+{
+	let srch = document.getElementById("searchContact").value;
+	document.getElementById("searchResult").innerHTML = "";
+
+	let contactList = "";
+
+	let tmp = { search: srch, userId: userId };
+	let jsonPayload = JSON.stringify(tmp);
+
+	let url = urlBase + '/SearchContacts.' + extension;
+
+	let xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try {
+		xhr.onreadystatechange = function () {
+			if (this.readyState == 4 && this.status == 200) {
+				document.getElementById("searchResult").innerHTML = "Contact(s) have been retrieved";
+				let jsonObject = JSON.parse(xhr.responseText);
+
+				for (let i = 0; i < jsonObject.results.length; i++) {
+					contactList += jsonObject.results[i];
+					if (i < jsonObject.results.length - 1) {
+						contactList += "<br />\r\n";
+					}
+				}
+
+				document.getElementsByTagName("p")[0].innerHTML = contactList;
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch (err) {
+		document.getElementById("contactSearchResult").innerHTML = err.message;
+	}
+
 }

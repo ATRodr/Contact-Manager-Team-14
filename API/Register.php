2f4c27333fd63ae52431d87cf605592a27bc703f
ATRodr <72571049+ATRodr@users.noreply.github.com>
2024-05-28 14:26:58 -0400

    Fixed identical username issue

--- API/Register.php
+++ API/Register.php
@@ -1,41 +1,56 @@
 <?php
 
-    $inData = getRequestInfo();
+$inData = getRequestInfo();
 
-    $firstName = $inData["firstName"];
-    $lastName = $inData["lastName"];
-    $login = $inData["login"];
-    $password = $inData["password"];
+$firstName = $inData["firstName"];
+$lastName = $inData["lastName"];
+$login = $inData["login"];
+$password = $inData["password"];
 
-    $conn = new mysqli("localhost", "Master", "Password123", "ContactManager");
-    if ($conn->connect_error)
+$conn = new mysqli("localhost", "Master", "Password123", "ContactManager");
+if ($conn->connect_error)
+{
+    returnWithError($conn->connect_error);
+}
+else
+{
+    // Check if login exists
+    $stmt = $conn->prepare("SELECT * FROM Users WHERE Login = ? ");
+    $stmt->bind_param("s", $login);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows > 0)
     {
-      returnWithError($conn->connect_error);
+        returnWithError("Login already exists");
     }
     else
     {
         $stmt = $conn->prepare("INSERT into Users (FirstName, LastName, Login, Password) VALUES(?,?,?,?)");
-    	$stmt->bind_param("ssss", $firstName, $lastName, $login, $password);
+        $stmt->bind_param("ssss", $firstName, $lastName, $login, $password);
         $stmt->execute();
-    	$stmt->close();
-    	$conn->close();
-    	returnWithError("");
+        returnWithError("");
     }
+    // Only need to close stuff once don't need to close for every connection
+    $stmt->close();
+    $conn->close();
+}
 
-    function getRequestInfo()
-    {
-      return json_decode(file_get_contents('php://input'), true);
-    }
+function getRequestInfo()
+{
+    return json_decode(file_get_contents('php://input'), true);
+}
 
-    function sendResultInfoAsJson($obj)
-    {
-      header('Content-type: application/json');
-      echo $obj;
-    }
+function sendResultInfoAsJson($obj)
+{
+    header('Content-type: application/json');
+    echo $obj;
+}
 
-    function returnWithError($err)
-    {
-      $retValue = '{"error":"' . $err . '"}';
-      sendResultInfoAsJson($retValue);
-    }
+function returnWithError($err)
+{
+    $retValue = '{"error":"' . $err . '"}';
+    sendResultInfoAsJson($retValue);
+}
+
 ?>
